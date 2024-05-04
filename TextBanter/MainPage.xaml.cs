61deb95b7beb92daed86cc1a2d400e250f8a0bca
siercks <108698268+siercks@users.noreply.github.com>
2024-05-04 18:42:11 -0500

    May 2024
    Can transmit text to database in realtime. Need to fix XAML formatting. Would like to add authentication, maybe OAuth.

---
+++ TextBanter/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace TextBanter
+{
+    public partial class MainPage : ContentPage
+    {
+        FirebaseClient firebaseClient = new FirebaseClient("https://textbantermaui-default-rtdb.firebaseio.com/");
+        public ObservableCollection<TextPost> TextPostsCollection { get; set; } = new ObservableCollection<TextPost>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            var textCollection = firebaseClient
+                .Child("TextPost")
+                .AsObservable<TextPost>()
+                .Subscribe((item) =>
+                {
+                    //GifPosts.Clear()
+                    if (item.Object != null)
+                    {
+                        TextPostsCollection.Add(new TextPost
+                        {
+                            Username = item.Object.Username,
+                            Text = item.Object.Text,
+                            Date = item.Object.Date
+                        });
+                    }
+                });
+        }
+
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            firebaseClient.Child("TextPost").PostAsync(new TextPost
+            {
+                Username = "User",
+                Text = PostEntry.Text,
+                Date = DateTime.Now
+            });
+            PostEntry.Text = string.Empty;
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ TextBanter/TextPost.cs
@@ -0,0 +1,9 @@
+﻿namespace TextBanter
+{
+    public class TextPost
+    {
+        public required string Username { get; set; }
+        public string? Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
